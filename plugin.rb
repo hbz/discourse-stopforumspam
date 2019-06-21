c499f3a293d2da074d62eab8998a14b75cfe37b9
Mike Singer <mike@singerscreations.com>
2019-06-21 00:22:35 -0400

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: discourse-stopforumspam
+# about: Checks StopForumSpam database to see if new users are known spammers
+# version: 0.0.1
+# authors: Mike Singer
+# site: https://github.com/singerscreations/discourse-stopforumspam
+
+enabled_site_setting :stopforumspam_enabled
